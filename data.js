2344fe65be85646a0d82344e9fc50e5c28f3415c
Puneet Shukla <106764262+puneetshukla14@users.noreply.github.com>
2024-02-06 14:40:42 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,28 @@
+
+let movies = [
+  {
+    name: "wanda vision",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+    image: "assets/img/slider 3.png",
+  },
+  {
+    name: "falcon and the winter soldier",
+    des: "Every fight begins with a failure! Braving against the odds, Manoj Kumar Sharma puts it all on the line to crack the nation’s much sought-after exam: UPSC.",
+    image: "assets/img/fail.jpeg",
+  },
+  {
+    name: "raya and the last dragon",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+    image: "assets/img/slider 4.png",
+  },
+  {
+    name: "12th Fail",
+    des: "Loki is an American television series created by Michael Waldron for the streaming service Disney+, based on Marvel Comics featuring the character of the same name. It is the third television series in the Marvel Cinematic Universe (MCU) produced by Marvel Studios",
+    image: "assets/img/slider 1.png",
+  },
+  {
+    name: "luca",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+    image: "assets/img/slider 5.png",
+  },
+];
