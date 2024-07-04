291dad8734f0182dc6afcabf6d2141c4600c5d10
Puneet Shukla <106764262+puneetshukla14@users.noreply.github.com>
2024-07-04 13:45:05 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,11 @@
 
+
+let movies = [
+  {
+    name: "Mirzapur Season-3",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+    image: "assets/img/Mirzapur.jpg",
+  },
+ 
+  
+];
