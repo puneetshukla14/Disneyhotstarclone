3041e6e5fc2ca5504eda7fbe6cb392353b36ec69
Puneet Shukla <106764262+puneetshukla14@users.noreply.github.com>
2024-07-04 13:54:19 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,11 +1,28 @@
 
-
 let movies = [
   {
-    name: "Mirzapur Season-3",
+    name: "wanda vision",
     des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
     image: "assets/img/Mirzapur.jpg",
   },
- 
-  
+  {
+    name: "Mirzapur Season 3",
+    des: "Mizapur 3 continues the saga of power struggles in Mirzapur, with Guddu navigating newfound authority while facing persistent threats. The series promises intense drama, intricate character arcs, and deep-seated vendettas as it explores the consequences of past actions.",
+    image: "assets/img/Mirzapur.jpg",
+  },
+  {
+    name: "raya and the last dragon",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+    image: "assets/img/slider 4.png",
+  },
+  {
+    name: "12th Fail",
+    des: "Loki is an American television series created by Michael Waldron for the streaming service Disney+, based on Marvel Comics featuring the character of the same name. It is the third television series in the Marvel Cinematic Universe (MCU) produced by Marvel Studios",
+    image: "assets/img/Mirzapur.jpg",
+  },
+  {
+    name: "luca",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
+    image: "assets/img/slider 5.png",
+  },
 ];
