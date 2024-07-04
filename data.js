8d4ebda917adbab1a076d73612b0bce06970075f
Puneet Shukla <106764262+puneetshukla14@users.noreply.github.com>
2024-07-04 15:42:59 +0530

    Update data.js

--- data.js
+++ data.js
@@ -11,15 +11,16 @@ let movies = [
     image: "assets/img/Mirzapur.jpg",
   },
   {
+    name: "Panchayat Season-3",
+    des: "A disheartened Abhishek has left Phulera because of his transfer. Pradhan Brij and Vikas consider his transfer unfair and are determined not to let the new secretary join.Although Prahlad is still mourning his son and has taken to drinking to cope with his loss, he too joins Brij and Vikas’ efforts by threatening the new secretary.",
+    image: "assets/img/panchayat.jpg",
+  },
+  {
     name: "Loki Season-3",
     des: "Marvel Studios’ “Loki” features the God of Mischief as he steps out of his brother’s shadow in a new Disney+ series that takes place after the events of “Avengers: Endgame.” Tom Hiddleston returns as the title character, joined by Owen Wilson, Gugu Mbatha-Raw, Sophia Di Martino.",
     image: "assets/img/lokspos.jpg",
   },
-  {
-    name: "12th Fail",
-    des: "Loki is an American television series created by Michael Waldron for the streaming service Disney+, based on Marvel Comics featuring the character of the same name. It is the third television series in the Marvel Cinematic Universe (MCU) produced by Marvel Studios",
-    image: "assets/img/Mirzapur.jpg",
-  },
+  
   {
     name: "luca",
     des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
