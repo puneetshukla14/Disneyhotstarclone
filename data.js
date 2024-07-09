9e516deb54a55b8a591675a42809853784ec5366
Puneet Shukla <106764262+puneetshukla14@users.noreply.github.com>
2024-07-09 17:07:58 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,4 +1,12 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-P7VBRN72YR"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
+  gtag('config', 'G-P7VBRN72YR');
+</script>
 let movies = [
   
   {
