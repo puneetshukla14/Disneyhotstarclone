18b2de6a017b040597e6c780b88a045b68c0b4da
Puneet Shukla <106764262+puneetshukla14@users.noreply.github.com>
2024-07-09 17:06:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-P7VBRN72YR"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-P7VBRN72YR');
+</script>
 const figlet=require("figlet");
 figlet=()
